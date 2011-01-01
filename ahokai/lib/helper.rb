5dce17badc66947d39c79a3684ebba9165c6b6da
Sho Hashimoto <hashimoto@shokai.org>
2011-01-01 17:17:23 +0900

    fix

--- ahokai/lib/helper.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'yaml'
-require 'mongo'
-require 'bson'
-
-@conf = YAML::load open(File.dirname(__FILE__)+'/../config.yaml')
-@mongo = Mongo::Connection.new(@conf['mongo_host'], @conf['mongo_port'])
-@db = @mongo[@conf['mongo_dbname']]
