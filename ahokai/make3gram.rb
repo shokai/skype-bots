b5976332db22892f6b4facbc551dc1e99de3c64e
Sho Hashimoto <hashimoto@shokai.org>
2011-01-02 14:27:23 +0900

    make 3gram multi users

--- ahokai/make3gram.rb
+++ ahokai/make3gram.rb
@@ -6,49 +6,53 @@ $KCODE = 'u'
 
 tagger = Igo::Tagger.new('/usr/local/share/ipadic')
 
-chats = @db['chat'].find({:from => @conf['me']})
 start_at = Time.now
 
-# 同一人物で60秒以内のpostは連結する
-joined = Array.new
-last_time = 0
-chats.each{|c|
-  if c['time']-last_time < 60
-    joined[joined.size-1] += "\n#{c['body']}"
-  else
-    joined << c['body']
-  end
-  last_time = c['time']
-}
+for user in @conf['source_users'] do
+  chats = @db['chat'].find({:from => user})
 
-chats = joined
-
-count = 0
-chats.each{|c|
-  tmp = c.split(/\n/).map{|i| tagger.wakati i}
-  words = Array.new
-  tmp.each{|i|
-    words << "\n" unless words.empty?
-    words << i
-  }
-  next if words.size < 3
-  for i in 0...words.size-3 do
-    count += 1
-    n = {'a' => words[i], 'b' => words[i+1], 'c' => words[i+2]}
-    tmp = @db['ngram'].find_one n
-    if tmp
-      n = tmp
-      n['count'] += 1
+  # 同一人物で60秒以内のpostは連結する
+  joined = Array.new
+  last_time = 0
+  chats.each{|c|
+    if c['time']-last_time < 60
+      joined[joined.size-1] += "\n#{c['body']}"
     else
-      n['count'] = 1
+      joined << c['body']
     end
-    n['head'] = true if i == 0
-    n['tail'] = true if i == words.size-4
-    @db['ngram'].save n
-  end
-  p words
-}
+    last_time = c['time']
+  }
+  
+  chats = joined
+  
+  count = 0
+  chats.each{|c|
+    tmp = c.split(/\n/).map{|i| tagger.wakati i}
+    words = Array.new
+    tmp.each{|i|
+      words << "\n" unless words.empty?
+      words << i
+    }
+    next if words.size < 3
+    for i in 0...words.size-3 do
+      count += 1
+      n = {'a' => words[i], 'b' => words[i+1], 'c' => words[i+2]}
+      tmp = @db['ngram'].find_one n
+      if tmp
+        n = tmp
+        n['count'] += 1
+      else
+        n['count'] = 1
+      end
+      n['head'] = true if i == 0
+      n['tail'] = true if i == words.size-4
+      @db['ngram'].save n
+    end
+    p words
+  }
+end
 
 puts "---finished!!"
 puts "#{Time.now.to_i - start_at.to_i} (sec)"
 puts "stored #{chats.count} chats / #{count} 3grams"
+
